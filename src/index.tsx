71498b36299cee423a160011dc49957a67a947e8
Fy5tew <nikturchinovich@mail.ru>
2024-12-23 23:03:03 +0300

    feat(constants): add html root containers constants

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import { ROOT_ELEMENT_ID } from '#constants/dom';
+
 import { Router } from './router';
 import { store } from './store';
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+    document.getElementById(ROOT_ELEMENT_ID) as HTMLElement
 );
 
 root.render(
