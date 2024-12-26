7449b4f4bbacdede13f4bd4a5e8a24e034cc883e
Fy5tew <nikturchinovich@mail.ru>
2024-12-26 22:56:55 +0300

    feat: add convertation methods to modal window

--- src/components/ConverterDialog/index.tsx
+++ src/components/ConverterDialog/index.tsx
@@ -1,11 +1,20 @@
-import { ComponentProps } from 'react';
+import { ChangeEvent, ComponentProps } from 'react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
+import { getConversionRateApi } from '#/api/getConversionRateApi';
+import { RootState } from '#/store';
 import { Dialog } from '#components/Dialog';
 import { CURRENCIES } from '#constants/quotes';
+import { formatPrice } from '#utils/formatPrice';
 
 import {
     CloseButton,
+    ErrorMessage,
+    InfoMessage,
     Input,
+    InputFormattedText,
+    InputWrapper,
     Label,
     LabelSubText,
     Option,
@@ -14,37 +23,108 @@ import {
     Wrapper,
 } from './styled';
 
+type CurrencyInputProps = ComponentProps<typeof Input> & {
+    value: number;
+};
+
 type ConverterDialogProps = ComponentProps<typeof Dialog> & {
     currencyCode: string;
 };
+
+function CurrencyInput({ value, children, ...props }: CurrencyInputProps) {
+    return (
+        <Label>
+            <InputWrapper>
+                <Input
+                    type="number"
+                    value={value}
+                    min={0}
+                    step={0.5}
+                    {...props}
+                />
+                <InputFormattedText>{formatPrice(value, 5)}</InputFormattedText>
+            </InputWrapper>
+            {children}
+        </Label>
+    );
+}
 
 export function ConverterDialog({
     currencyCode,
     onClose,
     ...props
 }: ConverterDialogProps) {
-    const selectedCurrency = CURRENCIES[currencyCode];
+    const fromCurrency = CURRENCIES[currencyCode];
+
+    const currencies = useSelector((state: RootState) =>
+        state.currency.currencies.filter(({ code }) => code !== currencyCode)
+    );
+
+    const [toCurrencyCode, setToCurrencyCode] = useState(currencies[0].code);
+    const [fromValue, setFromValue] = useState(0);
+    const [toValue, setToValue] = useState(0);
+    const [rate, setRate] = useState(1);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        setError('');
+        getConversionRateApi(fromCurrency.code, toCurrencyCode)
+            .then((rate) => setRate(rate))
+            .catch((error) => setError(error.message));
+    }, [fromCurrency.code, toCurrencyCode]);
+
+    useEffect(() => {
+        setToValue(fromValue * rate);
+    }, [fromValue, rate]);
+
+    const handleFromValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.valueAsNumber;
+        setFromValue(value);
+        setToValue(value * rate);
+    };
+
+    const handleToValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.valueAsNumber;
+        setToValue(value);
+        setFromValue(value / rate);
+    };
+
+    const handleToCurrencyChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        setToCurrencyCode(event.target.value);
+    };
 
     return (
         <Dialog {...props} onClose={onClose}>
             <Wrapper>
                 <Title>Currency Converter</Title>
-                <Label>
-                    <Input />
+                <CurrencyInput
+                    value={fromValue}
+                    onChange={handleFromValueChange}
+                >
                     <LabelSubText>
-                        {selectedCurrency.symbol} {selectedCurrency.title}
+                        {fromCurrency.symbol} {fromCurrency.title}
                     </LabelSubText>
-                </Label>
-                <Label>
-                    <Input />
-                    <Select>
-                        {Object.values(CURRENCIES).map((currency) => (
-                            <Option key={currency.code} value={currency.code}>
-                                {currency.symbol} {currency.title}
+                </CurrencyInput>
+                <CurrencyInput value={toValue} onChange={handleToValueChange}>
+                    <Select
+                        value={toCurrencyCode}
+                        onChange={handleToCurrencyChange}
+                    >
+                        {currencies.map(({ code, symbol, title }) => (
+                            <Option key={code} value={code}>
+                                {symbol} {title}
                             </Option>
                         ))}
                     </Select>
-                </Label>
+                </CurrencyInput>
+                {error ? (
+                    <ErrorMessage>{error}</ErrorMessage>
+                ) : (
+                    <InfoMessage>
+                        1 {fromCurrency.code} ≈ {formatPrice(rate, 2)}{' '}
+                        {toCurrencyCode}
+                    </InfoMessage>
+                )}
                 <CloseButton onClick={onClose}>Close</CloseButton>
             </Wrapper>
         </Dialog>
--- src/components/ConverterDialog/styled.ts
+++ src/components/ConverterDialog/styled.ts
@@ -22,13 +22,37 @@ export const Label = styled.label`
 
 export const LabelSubText = styled.span``;
 
+export const InputWrapper = styled.div`
+    position: relative;
+
+    &:has(input:focus) > span {
+        display: none;
+    }
+
+    &:not(:has(input:focus)) > input {
+        color: transparent;
+    }
+`;
+
 export const Input = styled.input`
     padding: 1em;
     border-radius: 0.5em;
+    width: 100%;
+
+    font-size: 1em;
 
     color: ${({ theme }) => theme.textColor};
     background-color: ${({ theme }) =>
         theme.themeName === 'dark' ? COLORS.onyx : COLORS.white};
+`;
+
+export const InputFormattedText = styled.span`
+    position: absolute;
+    top: 50%;
+    left: 1em;
+    transform: translateY(-50%);
+
+    font-size: 1em;
 `;
 
 export const Select = styled.select`
@@ -41,6 +65,20 @@ export const Select = styled.select`
 `;
 
 export const Option = styled.option``;
+
+export const InfoMessage = styled.p`
+    color: ${COLORS.greenTeal};
+    min-height: 1.5em;
+    line-height: 1.5em;
+    text-align: center;
+`;
+
+export const ErrorMessage = styled.p`
+    color: ${COLORS.deepCarminePink};
+    min-height: 1.5em;
+    line-height: 1.5em;
+    text-align: center;
+`;
 
 export const CloseButton = styled.button`
     padding: 1em;
