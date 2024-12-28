86a00246b8c6f0fbc99dd0793c7cb6900b670881
Fy5tew <nikturchinovich@mail.ru>
2024-12-28 21:53:00 +0300

    fix(styles): fix details interactive zone size

--- src/components/Details/styled.ts
+++ src/components/Details/styled.ts
@@ -4,13 +4,9 @@ import chevronIcon from '#assets/chevron.svg';
 import { COLORS, MEDIA } from '#constants/styles';
 
 export const Details = styled.details`
-    display: flex;
-    flex-direction: column;
-    gap: 2em;
-
     &[open] {
         & > summary::before {
-            transform: translateY(-50%) rotate(180deg);
+            transform: translateY(calc(-50% - 1em)) rotate(180deg);
         }
     }
 
@@ -21,8 +17,8 @@ export const Details = styled.details`
 
 export const Summary = styled.summary`
     position: relative;
-
     list-style: none;
+    padding-bottom: 2em;
 
     &::before {
         content: '';
@@ -36,7 +32,7 @@ export const Summary = styled.summary`
         -webkit-mask-size: contain;
         mask: url('${chevronIcon}') no-repeat center;
         mask-size: contain;
-        transform: translateY(-50%);
+        transform: translateY(calc(-50% - 1em));
         transition: transform 0.5s ease-in-out;
     }
 
