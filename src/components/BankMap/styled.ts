7dd9de6eef4e76395c7d7df3cc91697354d5463b
Fy5tew <nikturchinovich@mail.ru>
2024-12-30 23:13:27 +0300

    feat(map): add map components

---
+++ src/components/BankMap/styled.ts
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+export const MapWrapper = styled.div`
+    height: 600px;
+    border-radius: 1em;
+    overflow: hidden;
+
+    & > * {
+        width: 100%;
+        height: 100%;
+    }
+`;
