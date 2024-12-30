59ad484e74e38bb08c6e6349d10737b42d89b61e
Fy5tew <nikturchinovich@mail.ru>
2024-12-31 02:52:30 +0300

    feat(hooks): add useOnClickOutside hook

---
+++ src/hooks/useOnCLickOutside.ts
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+
+export function useOnClickOutside(
+    ref: React.RefObject<HTMLElement>,
+    handler: (event: TouchEvent | MouseEvent) => void
+) {
+    useEffect(() => {
+        const listener = (event: TouchEvent | MouseEvent) => {
+            if (
+                !ref.current ||
+                ref.current.contains(event.target as HTMLElement)
+            ) {
+                return;
+            }
+            handler(event);
+        };
+        document.addEventListener('mousedown', listener);
+        document.addEventListener('touchstart', listener);
+        return () => {
+            document.removeEventListener('mousedown', listener);
+            document.removeEventListener('touchstart', listener);
+        };
+    }, [ref, handler]);
+}
